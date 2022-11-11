87c743c42fae2834344f3e67f11813679d90a7e9
rejaul-karim10 <rejalkarim66666@gmail.com>
2022-11-11 23:32:20 +0800

    fixed some bugs

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -19,12 +19,39 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     const serviceCollection = client.db("cloudKitchen").collection("services");
+// for home page
+    app.get("/home-services", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query).limit(3);
+      const service = await cursor.toArray();
+      console.log(service)
+      res.send(service);
+    });
+
     app.get("/services", async (req, res) => {
       const query = {};
       const cursor = serviceCollection.find(query);
-      const services = await cursor.toArray();
-      res.send(services);
+      const service = await cursor.toArray();
+      console.log(service)
+      res.send(service);
     });
+
+    app.get('/services/:id', async (req, res)=>{
+      const id = req.params.id;
+      const query = {_id: ObjectId(id)}
+      const service = await serviceCollection.findOne(query)
+      res.send(service);
+    })
+
+    app.post("/services", async (req, res) => {
+      const services = req.body;
+      const result = await serviceCollection.insertOne(services);
+      res.send(result);
+    });
+
+
+
+    
   } finally {
   }
 }
